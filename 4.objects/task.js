3d062fe29f8c9bdf9de61c648a7a6272de28da79
burchakov1998 <burchakov1998@gmail.com>
2023-06-13 23:57:27 +0300

    modifed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,62 +1,45 @@
-"use strict"
-
 
 function Student(name, gender, age) {
-   this.name = name;
-   this.gender = gender;
-   this.age = age;
-   this.marks = [];
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
-
 let student1 = new Student("Василиса", "женский", 19);
 let student2 = new Student("Артём", "мужской", 25);
 
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
 
-Student.prototype.setSubject = function(subjectName) {
-   this.subject = subjectName;
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks != undefined) {
+    this.marks.push(...marks);
+  }
+}
+Student.prototype.getAverage = function () {
+  let sum, result = 0;
+
+  if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+  } else {
+    this.marks.forEach(element => {
+      sum += element;
+      return result = sum / this.marks.length;
+
+    });
+
+  }
+
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
 student1.setSubject("Algebra");
-student2.setSubject("Geometry");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
 
-Student.prototype.addMarks = function(...marksToAdd) {
-   this.marks = marksToAdd;
-}
-
-if(student1.hasOwnProperty("marks")){
-   student1.addMarks(4, 5, 4, 5);
-}
-
-if(student2.hasOwnProperty("marks")){
-   student2.addMarks(4, 4, 4, 5);
-}
-
-Student.prototype.getAverage = function() {
-   let sum = 0;
-   if(this.marks.length != 0) {
-       for(let i = 0; i < this.marks.length; i++) {
-           sum += this.marks[i];
-       }
-       console.log(sum / this.marks.length);
-   }else {
-       console.log(sum);
-   }
-
-}
-
-if(student1.hasOwnProperty("marks")) {
-   student1.getAverage();
-}
-
-if(student2.hasOwnProperty("marks")) {
-   student2.getAverage();
-}
-
-Student.prototype.exclude = function(reason) {
-   delete this.subject;
-   delete this.marks;
-   this.exclude = reason;
-}
-
-student2.exclude("Плохая учеба");
-console.log(student1);
-console.log(student2);
