cd9d6faeaac09709071c0115f1a3381f84ef2b0f
burchakov1998 <burchakov1998@gmail.com>
2023-07-04 17:03:16 +0300

    modifed

--- 7.async/task.js
+++ 7.async/task.js
@@ -6,24 +6,36 @@ class AlarmClock {
 
     addClock  (time, CBfunc) {
 //      console.log(arguments);
-        if((arguments.length != 2) || (time === null) || (CBfunc === null)) {
-            throw new Error('Отсутствуют обязательные аргументы');
-        }
-        else {
-          let ind;
-       //  console.log(this.alarmCollection);
-          if(this.alarmCollection.length < 0) {
-            ind = this.alarmCollection.find(function (item) {
-                return item.time === time;
-            }); this.alarmCollection.filter((call) => call.time === time);
-          }
-          if(ind) {
-            console.warn('Уже присутствует звонок на это же время');
-          }
-          else {
-            this.alarmCollection.push({callback: CBfunc, time: time, canCall: true});
-          }
-       }
+let ind;
+if(time ===  undefined || CBfunc === undefined){
+  throw new Error('Отсутствуют обязательные аргументы');
+}
+else if(ind > 0){
+  console.warn('Уже присутствует звонок на это же время');
+
+}
+else{
+  this.alarmCollection.push({callback: CBfunc, time: time, canCall: true});
+
+}
+      //   if((arguments.length != 2) || (time === null) || (CBfunc === null)) {
+      //       throw new Error('Отсутствуют обязательные аргументы');
+      //   }
+      //   else {
+      //     let ind;
+      //  //  console.log(this.alarmCollection);
+      //     if(this.alarmCollection.length < 0) {
+      //       ind = this.alarmCollection.find(function (item) {
+      //           return item.time === time;
+      //       }); this.alarmCollection.filter((call) => call.time === time);
+      //     }
+      //     if(ind) {
+      //       console.warn('Уже присутствует звонок на это же время');
+      //     }
+      //     else {
+      //       this.alarmCollection.push({callback: CBfunc, time: time, canCall: true});
+      //     }
+      //  }
     }
 
     removeClock (time) {
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,47 +1,43 @@
 function cachingDecoratorNew(func) {
-    let cache = [];
-  
+  let cache = [];
+
   function wrapper(...args) {
-      const hash = args.join(','); // получаем правильный хэш
+  const hash = args.join(','); // получаем правильный хэш
       let idx = cache.findIndex((item)=> item.hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
       if (idx !== -1 ) { // если элемент не найден
-          console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+           console.log(md5("Из кэша: " + cache[idx].result)); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
           return "Из кэша: " + cache[idx].result;
-      }
+       }
   
       let result = func(...args); // в кэше результата нет - придётся считать
-      cache.push({hash, result}) ; // добавляем элемент с правильной структурой
-      if (cache.length > 5) { 
-        cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
-      }
-      console.log("Вычисляем: " + result);
-      return "Вычисляем: " + result;  
+       cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+       if (cache.length > 5) { 
+         cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
+       }
+       console.log(md5("Вычисляем: " + result));
+       return "Вычисляем: " + result;  
+   }
+
+   return wrapper;
+   
+   }
+   let sum = (a, b) => a + b;
+function makeCounter(func,ms){
+   wrapper.count = 0;
+  wrapper.allcount = [];
+   function wrapper(...args){
+     setTimeout(function(){
+        let savedThis = this;
+       func.apply(savedThis, args);
+     },ms)
+     wrapper.count++;
+     wrapper.allcount.push(wrapper.count);
+    console.log (`Функция вызвана  ${wrapper.allcount.length}`);  
+  // console.log (`count equals ${wrapper.count}`); 
+    return func(...args)            
   }
   return wrapper;
-  }
-  
-  
-  function debounceDecoratorNew(func, ms) {  
-    let timerId = null;
-    function wrapper (...args){
-      if (timerId === null) {
-        func(...args);
-      }
-      clearTimeout(timerId);
-      timerId = setTimeout(() => timerId = null, ms);
-    }
-    return wrapper;
-  } 
-  
-  function debounceDecorator2(func, ms) {
-    let timerId = null;
-    function wrapper(...args){
-      if (timerId === null) {
-        func(...args);
-      }
-      clearTimeout(timerId);
-      timerId = setTimeout(() => timerId = null, ms);
-      wrapper.count++;
-    }
-    return wrapper;
-  }
+}
+let a = makeCounter(sum);
+setTimeout( () => a(1,2), 100)
+ setTimeout( () => a(1,2), 2000) 
